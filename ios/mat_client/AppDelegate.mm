f803601bceb23f2dea33683705f07b3b94aac09d
wgj <wj61@duke.edu>
2023-06-13 15:41:39 +0900

    initial map main

--- ios/mat_client/AppDelegate.mm
+++ ios/mat_client/AppDelegate.mm
@@ -1,11 +1,14 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
+#import <GoogleMaps/GoogleMaps.h>
 
 @implementation AppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+- (BOOL)application:(UIApplication *)application
+didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    [GMSServices provideAPIKey:@"AIzaSyDz9QSmJrH-ryPP9KJ6yGcWTpVCN83HqpA"];
   self.moduleName = @"mat_client";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
