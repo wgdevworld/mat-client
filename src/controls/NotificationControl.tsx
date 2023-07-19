d40eebd9ede34720e0b5a52329bda7f799e3787b
wgj <wj61@duke.edu>
2023-07-19 18:15:03 +0900

    알람 기능 추가

--- src/controls/NotificationControl.tsx
+++ src/controls/NotificationControl.tsx
@@ -1,14 +1,20 @@
 import messaging from '@react-native-firebase/messaging';
+import notifee from '@notifee/react-native';
 
 export const initPushNotification = async () => {
   // Register the device with FCM
   try {
-    await messaging().registerDeviceForRemoteMessages();
-
     // Get the token
     const token = await messaging().getToken();
-
     console.log('Token: ' + token);
+
+    // Register background handler
+    messaging().setBackgroundMessageHandler(async remoteMessage => {
+      onDisplayNotification(
+        remoteMessage.notification?.body,
+        remoteMessage.notification?.title,
+      );
+    });
   } catch (e) {
     console.log(e);
   }
@@ -16,4 +22,31 @@ export const initPushNotification = async () => {
   //TODO: post to server
   //   // Save the token
   //   await postToApi('/users/1234/tokens', {token});
+};
+
+export const onDisplayNotification = async (
+  message?: string,
+  title?: string,
+) => {
+  // Request permissions (required for iOS)
+  await notifee.requestPermission();
+
+  // // Create a channel (required for Android)
+  // const channelId = await notifee.createChannel({
+  //   id: 'default',
+  //   name: 'Default Channel',
+  // });
+
+  await notifee.displayNotification({
+    title: title,
+    body: message,
+    // android: {
+    //   channelId,
+    //   smallIcon: 'name-of-a-small-icon', // optional, defaults to 'ic_launcher'.
+    //   // pressAction is needed if you want the notification to open the app when pressed
+    //   pressAction: {
+    //     id: 'default',
+    //   },
+    // },
+  });
 };
