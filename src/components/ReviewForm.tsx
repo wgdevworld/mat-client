b5b05d6e69784620ff070c55812113a751da353f
Changmin Shin <cs521@duke.edu>
2023-08-04 15:11:18 +0900

    changed rating star package

--- src/components/ReviewForm.tsx
+++ src/components/ReviewForm.tsx
@@ -1,12 +1,12 @@
 import React, {useState} from 'react';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import {Rating} from 'react-native-ratings';
-import assets from '../../assets';
+// import assets from '../../assets';
+import StarRating from 'react-native-star-rating-widget';
 import {
   View,
   Text,
   StyleSheet,
-  Image,
+  // Image,
   TextInput,
   TouchableOpacity,
 } from 'react-native';
@@ -19,30 +19,42 @@ import {
 //   //   images: Image[];
 // }
 
-const ReviewForm = (
-  {
-    //   date,
-    //   images,
-  },
-) => {
-  const images = [
-    assets.images.스시올로지,
-    assets.images.야키토리나루토,
-    assets.images.월량관,
-  ];
+// class ReviewForm extends Component {
+//   ratingCompleted(rating: any) {
+//     console.log(rating);
+//   }
 
-  const [getRating, setRating] = useState(0);
-  function ratingCompleted(rating: any) {
-    setRating(rating);
-    console.log(getRating);
-  }
+const ReviewForm = () => {
+  // const images = [
+  //   assets.images.스시올로지,
+  //   assets.images.야키토리나루토,
+  //   assets.images.월량관,
+  // ];
+  // render()
+  const handlePressSumbit = () => {
+    // add data to db and rerender
+    console.log(comment);
+    console.log(rating);
+  };
+  const [rating, setRating] = useState(0);
+  const [comment, setComment] = useState('');
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>리뷰 작성하기</Text>
       {/* <View style={styles.icon}>
         <Ionicons name="star-outline" size={15} color={'white'} />
       </View> */}
-      <Rating
+      <StarRating
+        rating={rating}
+        onChange={setRating}
+        enableHalfStar
+        enableSwiping
+        maxStars={5}
+        starSize={20}
+        //minRating
+      />
+      {/* <Rating
         imageSize={20}
         ratingCount={5}
         style={{paddingVertical: 10}}
@@ -51,8 +63,8 @@ const ReviewForm = (
         jumpValue={0.5}
         startingValue={0}
         minValue={1}
-        onFinishRating={ratingCompleted}
-      />
+        onFinishRating={this.rating => setRating(rating)}
+      /> */}
       <View style={styles.inputContainer}>
         <View style={styles.icon}>
           <Ionicons name="text" size={15} color={'white'} />
@@ -63,73 +75,16 @@ const ReviewForm = (
           placeholderTextColor="white"
           selectionColor="white"
           multiline
+          onChangeText={text => setComment(text)}
         />
       </View>
-      {/* useState to store data, add onPress to TouchableOpacity */}
-      <TouchableOpacity
-        style={{
-          marginBottom: 5,
-          paddingHorizontal: 10,
-          paddingVertical: 5,
-          borderRadius: 20,
-          backgroundColor: 'white',
-        }}>
-        <Text
-          style={{
-            color: 'black',
-
-            backgroundColor: 'white',
-          }}>
-          등록하기
-        </Text>
+      {/* add onPress to TouchableOpacity */}
+      <TouchableOpacity style={styles.submitButton} onPress={handlePressSumbit}>
+        <Text style={styles.submitText}>등록하기</Text>
       </TouchableOpacity>
-      {/* <View style={styles.inputContainer}>
-        <View style={styles.icon}>
-          <Ionicons name="star-outline" size={15} color={'white'} />
-        </View>
-        <TextInput
-          style={styles.input}
-          placeholder="비밀번호"
-          placeholderTextColor="white"
-          selectionColor="white"
-        />
-      </View> */}
-      {/* <TouchableOpacity style={styles.forgotIdButton}>
-        <Text style={styles.forgotIdButtonText}>아이디 찾기</Text>
-      </TouchableOpacity> */}
-      {/* <View style={styles.forgotButtons}>
-        <TouchableOpacity style={styles.forgotIdButton}>
-          <Text style={styles.forgotIdButtonText}>아이디 찾기</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.forgotPasswordButton}>
-          <Text style={styles.forgotPasswordButtonText}>비밀번호 찾기</Text>
-        </TouchableOpacity>
-      </View>
-      <TouchableOpacity style={styles.loginButton}>
-        <Text style={styles.loginButtonText}>로그인</Text>
-      </TouchableOpacity>
-      <View style={styles.orContainer}>
-        <View style={styles.orLine} />
-        <Text style={styles.orText}>또는</Text>
-        <View style={styles.orLine} />
-      </View>
-      <TouchableOpacity style={styles.setAccountButton}>
-        <Text style={styles.setAccountButtonText}>회원가입</Text>
-      </TouchableOpacity>
-      <TouchableOpacity>
-        <Image source={assets.images.kakao_login_medium_narrow} />
-      </TouchableOpacity> */}
     </View>
-    // <View style={styles.container}>
-    //   <Text>{author}</Text>
-    //   <Text>{rating}</Text>
-    //   <Text>{content}</Text>
-    //   {/* <Text>{date.toLocaleDateString('ko-KR')}</Text> */}
-    //   {/* <ImageCarousel images={images} /> */}
-    // </View>
   );
 };
-
 const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
@@ -150,37 +105,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     flexDirection: 'row',
   },
-  mapName: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 5,
-    color: 'white',
-  },
-  mapAuthor: {
-    fontSize: 12,
-    marginBottom: 5,
-    color: 'white',
-  },
-  followersCount: {
-    color: 'white',
-    marginBottom: 10,
-  },
-  lowerHalf: {
-    flex: 1,
-    alignItems: 'flex-start',
-    justifyContent: 'center',
-    paddingHorizontal: 12,
-    paddingTop: 12,
-    width: '100%',
-    borderBottomLeftRadius: 20,
-    borderBottomRightRadius: 20,
-  },
-  //   container: {
-  //     flex: 1,
-  //     backgroundColor: '#FF4000',
-  //     alignItems: 'center',
-  //     justifyContent: 'center',
-  //   },
   text: {
     fontSize: 20,
     fontWeight: 'bold',
@@ -207,6 +131,18 @@ const styles = StyleSheet.create({
     borderBottomColor: '#eee',
     fontSize: 16,
     color: 'white',
+  },
+  submitButton: {
+    marginBottom: 5,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 20,
+    backgroundColor: 'white',
+  },
+  submitText: {
+    color: 'black',
+
+    backgroundColor: 'white',
   },
 });
 
