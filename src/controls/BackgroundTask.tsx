4a290264aa0396b707e0cd48a61cd440472d4b5b
wgj <wj61@duke.edu>
2023-11-03 15:40:26 -0400

    stable build with background geolocation

--- src/controls/BackgroundTask.tsx
+++ src/controls/BackgroundTask.tsx
@@ -1,6 +1,9 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const performYourBackgroundTask = async (taskId: string) => {
   try {
+    // obtain background location
+    // iterate through array and check distances. if distance is smaller than certain number, then add to list
+    // send notification for list.
     const response = await fetch('https://your.api/endpoint');
     const data = await response.json();
     console.log('Data fetched', data);
